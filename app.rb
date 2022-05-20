58e697425562a90e32aac2d194fb670c1da882ba
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-05-20 09:44:47 +0200

    slumparen klar

--- app.rb
+++ app.rb
@@ -2,13 +2,32 @@
 require 'sinatra'
 require 'slim'
 
-burger_arr = []
+$burger_hash = Hash.new
+
+def randomizer(hash)
+    burger_array = []
+    if hash.class != NilClass
+        hash.map do |ingredient|
+            for i in 0..(ingredient[1].to_i - 1)
+                burger_array << ingredient[0]
+            end
+        end
+        burger_array = burger_array.shuffle
+        burger_array = burger_array.insert(0, "bread_top")
+        burger_array = burger_array.insert(-1, "bread_bot")
+        return burger_array
+    else
+        return []
+    end
+end
 
 get('/') do
-    slim(:index, locals:{burger_array:burger_arr})
+    slim(:index, locals:{burger_array:randomizer($burger_hash)})
 end
 
 post('/generateburger') do
-    burger_arr = []
+    params[:bread] = params[:bread].to_i - 2
+    $burger_hash = params
+    p randomizer($burger_hash)
     redirect('/')
 end
